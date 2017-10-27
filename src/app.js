59001f31eda823a621e27899efadb7ecb6ec69eb
Oleksandr Panibratenko <Oleksandr_Panibratenko@epam.com>
2017-10-27 12:41:51 +0300

    react connected to redux

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 "use strict"
 import React from 'react';
 import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+
 import {applyMiddleware, createStore} from 'redux';
 import logger from 'redux-logger';
-//STEP 3 define reducers
+
 import reducers from './reducers/index';
 
 import {addToCart} from './actions/cartActions';
@@ -11,19 +13,18 @@ import {postBooks, deleteBooks, updateBooks} from './actions/booksActions';
 
 import BooksList from './components/pages/booksList';
 
-// STEP 1 create the store
+
 const middeleware = applyMiddleware(logger);
 const store = createStore(reducers, middeleware);
 store.subscribe(function() {
   //console.log('current state is: ', store.getState());
-  //console.log('current price: ', store.getState().price);
 });
-// STEP 2 create and dispatch actions
 
 render(
-  <BooksList />, document.getElementById('app')
+  <Provider store = {store} >
+    <BooksList />
+  </Provider>, document.getElementById('app')
 );
-
 
 store.dispatch(postBooks(
     [{
@@ -43,37 +44,12 @@ store.dispatch(postBooks(
     }]
 ));
 
-store.dispatch(postBooks(
-    [{
-        id: 3,
-        title: '3this is the book title3',
-        description: '3this is the book description3 ',
-        price: 77.77
-    }]
-));
-
-
-store.dispatch(deleteBooks({id:3}));
-store.dispatch(deleteBooks({id:2}));
-store.dispatch(deleteBooks({id:23}));
-
-store.dispatch(postBooks(
-    [{
-        id: 4,
-        title: '4444 4 title',
-        description: 'empty',
-        price: 44.44
-    }]
-));
-
+//store.dispatch(deleteBooks({id:2}));
 store.dispatch(updateBooks(
     {
-        id: 4,
-        title: 'Learn React in 221212124h',
-        //description: 'updated description'
+        id: 1,
+        title: 'Learn React in 24h'
     }
 ));
-
 // ADD TO CART
-store.dispatch(addToCart([{id:11}]));
-store.dispatch(addToCart([{id:22}]));
+store.dispatch(addToCart([{id:1}]));
--- src/components/pages/booksList.js
+++ src/components/pages/booksList.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 class BooksList extends React.Component {
   render() {
+    const booksList = this.props.books.map((arrayBook) => {
+      return (
+        <div key={arrayBook.id}>
+          <h3>Title: {arrayBook.title}</h3>
+          <h4>Description: {arrayBook.description}</h4>
+          <h4>price: {arrayBook.price}</h4>
+        </div>
+      )
+    })
     return (
       <div>
         <h1> Books List </h1>
+        {booksList}
       </div>
     )
   }
 }
 
-export default BooksList;
+function mapStateToProps(state){
+  return {
+    books: state.books.books
+  }
+}
+export default connect(mapStateToProps)(BooksList);
