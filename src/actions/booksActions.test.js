88b27fd3d31e1750e9a114e4e040571322a9df13
Oleksandr Panibratenko <Oleksandr_Panibratenko@epam.com>
2017-11-07 15:59:56 +0200

    Jest tests covered books actions and reducers

--- src/actions/booksActions.test.js
+++ src/actions/booksActions.test.js
@@ -1,13 +1,58 @@
 import {getBooks, postBooks, deleteBooks, updateBooks} from './booksActions';
 
-
 describe('Book Actions', () => {
 
   test('Should return GET_BOOKS action', () => {
-    expect(getBooks()).toEqual({
-        type:"GET_BOOKS"
-    });
+    expect(getBooks()).toEqual({type: "GET_BOOKS"});
+  });
 
+  test('Should return POST_BOOK action with book object in array', () => {
+    const book = {
+      id: 77,
+      title: '777 book title',
+      description: '777 description ',
+      price: 77777
+    }
+    expect(postBooks([book])).toEqual({
+      type: "POST_BOOK",
+      payload: [
+        {
+          id: 77,
+          title: '777 book title',
+          description: '777 description ',
+          price: 77777
+        }
+      ]
+    });
+  });
+
+  test('Should return DELETE_BOOK action with id object', () => {
+    expect(deleteBooks({id: "123abc"})).toEqual({
+      type: "DELETE_BOOK",
+      payload: {
+        id: '123abc'
+      }
+    });
+  });
+
+  test('Should return UPDATE_BOOK action with book object', () => {
+    const book = {
+      id: 1,
+      title: 'First book title',
+      description: '1st description ',
+      price: 1000
+    }
+    expect(updateBooks([book])).toEqual({
+      type: "UPDATE_BOOK",
+      payload: [
+        {
+          id: 1,
+          title: 'First book title',
+          description: '1st description ',
+          price: 1000
+        }
+      ]
+    });
   });
 
 })
---
+++ src/reducers/booksReducers.test.js
@@ -0,0 +1,127 @@
+import {booksReducers} from './booksReducers';
+
+const state = {
+  books:[
+    {
+        _id: 1,
+        title: 'First Book',
+        description: 'this is the book description ',
+        price: 10.25
+    },
+    {
+      _id: 2,
+      title: 'Second',
+      description: 'Description for Second',
+      price: 22.88
+    },
+    {
+        _id: 3,
+        title: 'Learn React',
+        description: '4424h and you winner!!!',
+        price: 300.30
+    }
+
+  ]
+}
+
+describe('Book Reducer', () => {
+
+  test('Should return initial values of books in case of GET_BOOKS', () => {
+    const newState = booksReducers(undefined, {type: "GET_BOOKS"});
+    expect(newState).toEqual({
+       books:[...state.books]
+    });
+  });
+
+
+  test('Should add new book in case of POST_BOOK', () => {
+    const book = {
+      _id: 77,
+      title: '777 book title',
+      description: '777 description ',
+      price: 77777
+    };
+    const action = {
+      type: "POST_BOOK",
+      payload: [ book
+      ]
+    }
+    const newState = booksReducers(state, action);
+    expect(newState).toEqual({
+       books:[...state.books, book]
+    });
+  });
+
+
+  test('Should delete a book in case of DELETE_BOOK', () => {
+    const action = {
+      type: "DELETE_BOOK",
+      payload: {_id: 1 }
+    }
+    const newState = booksReducers(state, action);
+    expect(newState).toEqual({
+      books:[
+        state.books[1],
+        state.books[2]
+      ]
+    });
+  });
+
+  test('Should NOT delete any book with wrong id in case of DELETE_BOOK', () => {
+    const action = {
+      type: "DELETE_BOOK",
+      payload: {_id: -1 }
+    }
+    const newState = booksReducers(state, action);
+    expect(newState).toEqual(state);
+  });
+
+  test('Should updete a book in case of UPDATE_BOOK', () => {
+    const action = {
+      type: "UPDATE_BOOK",
+      payload: {
+        _id: 1,
+        title: 'Was First Book',
+        description: 'Was this is the book description ',
+        price: 999.99
+       }
+    }
+    const newState = booksReducers(state, action);
+    expect(newState).toEqual({
+        books:[
+          {
+              _id: 1,
+              title: 'Was First Book',
+              description: 'this is the book description ',
+              price: 10.25
+          },
+          state.books[1],
+          state.books[2]
+        ]
+
+    });
+  });
+
+  test('Should NOT update any book with wrong id in case of UPDATE_BOOK', () => {
+    const action = {
+      type: "UPDATE_BOOK",
+      payload: {
+        _id: -1,
+        title: 'Any',
+        description: 'Any ',
+        price: 0.99
+       }
+    }
+    const newState = booksReducers(state, action);
+    expect(newState).toEqual({
+        books:[
+          state.books[0],
+          state.books[1],
+          state.books[2]
+        ]
+
+    });
+  });
+
+
+})
