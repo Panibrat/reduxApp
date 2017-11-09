090050c1569589a22de92a0ba2fed9f417949e7e
Oleksandr Panibratenko <Oleksandr_Panibratenko@epam.com>
2017-11-09 10:41:28 +0200

    first enzyme test

--- src/components/pages/Header.test.js
+++ src/components/pages/Header.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+//import ReactShallowRenderer from 'react-test-renderer/shallow';
+import {shallow} from 'enzyme';
 import Header from './Header';
 
 test('should render Header correctly', () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
+  const wrapper = shallow(<Header />);
+  expect(wrapper.find('h1').length).toBe(1);
+  expect(wrapper.find('h1').text()).toBe('Header');
+
+
+  // const renderer = new ReactShallowRenderer();
+  // renderer.render(<Header />);
+  // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
