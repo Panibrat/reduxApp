6d09c2c0365b30204eafaf5c6c4dc4cb0a0291e8
Oleksandr Panibratenko <Oleksandr_Panibratenko@epam.com>
2017-11-09 11:23:59 +0200

    Enzyme snapshot static test

--- src/components/pages/Header.test.js
+++ src/components/pages/Header.test.js
@@ -1,15 +1,12 @@
 import React from 'react';
-//import ReactShallowRenderer from 'react-test-renderer/shallow';
 import {shallow} from 'enzyme';
 import Header from './Header';
 
 test('should render Header correctly', () => {
   const wrapper = shallow(<Header />);
   expect(wrapper.find('h1').length).toBe(1);
-  expect(wrapper.find('h1').text()).toBe('Header');
+  expect(wrapper.find('h1').text()).toBe('Header Enzyme');
 
+  expect(wrapper).toMatchSnapshot();
 
-  // const renderer = new ReactShallowRenderer();
-  // renderer.render(<Header />);
-  // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
